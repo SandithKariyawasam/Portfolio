cb4704a3de90ff3ec3cde59b5105cd6f2b4cc327
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-05-10 18:04:15 +0530

    Update App.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,7 +4,7 @@ import '../src/assets/css/bootstrap.min.css'
 import '../src/assets/css/aos.css'
 import '../src/assets/css/style.css'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 import Home from './pages/home'
