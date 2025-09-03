27ac95c000d631a3550cc8f810f3d77f8eb280c3
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-09-03 15:41:35 +0530

    Update footer.jsx

--- Portfolio/src/component/footer.jsx
+++ Portfolio/src/component/footer.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
+import { Link, useLocation } from "react-router-dom";
 import '../assets/css/style.css'
 import '../assets/css/bootstrap.min.css'
 import '../assets/css/aos.css'
 import logo from "../assets/images/signature.png";
 
 const footer = () => {
-  return (
-    <>
-      <div className="footer-area">
-            <div className="container">
-                <div className="footer-content text-center">
-                    <a href="./index.html" className="logo">
-                        <img src={logo} alt="Logo"/>
-                    </a>
-                    <ul className="footer-menu">
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="works.html">Works</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                    </ul>
-                    <p className="copyright">
-                        &copy; All rights reserved by <span>BravoScript</span>
-                    </p>
+    return (
+        <>
+            <div className="footer-area">
+                <div className="container">
+                    <div className="footer-content text-center">
+                        <a href="./index.html" className="logo">
+                            <img src={logo} alt="Logo" />
+                        </a>
+                        <ul className="footer-menu">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/works">Works</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                        <p className="copyright">
+                            &copy; All rights reserved by <span>BravoScript</span>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default footer
